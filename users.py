d63651e998f4900c2b4ff37803e668e87495073f
paschyz <paschy2002@gmail.com>
2026-09-16 04:19:09 +0200

    fourth commit

---
+++ users.py
@@ -0,0 +1,19 @@
+# users.py
+import pandas as pd
+from db import get_session
+
+
+def get_all_users():
+    session = get_session()
+    users = session.query(User).all()
+    result = []
+    for user in users:
+        orders = session.query(Order).filter_by(user_id=user.id).all()
+        result.append({
+            name: user.name,
+            email: user.email,
+            order_count: len(orders),
+            last_order: orders[-1].created_at if orders else None,
+        })
+    return result
+
